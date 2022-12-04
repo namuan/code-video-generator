d8fccff8b56caab22b945441099a91828338c13e
namuan <575441+namuan@users.noreply.github.com>
2022-12-04 21:33:55 +0000

    feat: Load icons from font-awesome

--- examples/svg-load-transform.py
+++ examples/svg-load-transform.py
@@ -5,14 +5,24 @@ from code_video import CodeScene
 
 example_dir = dirname(__file__)
 
+REGULAR_FONT_AWESOME = f"{example_dir}/resources/font-awesome/svgs/regular"
+SOLID_FONT_AWESOME = f"{example_dir}/resources/font-awesome/svgs/solid"
+BRANDS_FONT_AWESOME = f"{example_dir}/resources/font-awesome/svgs/brands"
+
+
+def get_svg_object(icon_file: str):
+    return SVGMobject(icon_file)
+
+
 class SvgLoadTransform(CodeScene):
     def construct(self):
-        # load svg from resources/app.svg into manim object
+        self.camera.background_color = WHITE
         svg = SVGMobject(f"{example_dir}/resources/app.svg")
         self.play(Create(svg))
         self.play(svg.animate.scale(0.3).to_edge(UL))
-        company_label = Text("DeskRiders", font="Montserrat").scale(0.5).next_to(svg, RIGHT)
-        self.play(Create(company_label))
+        company_label = Text("DeskRiders", color=BLACK).scale(0.5).next_to(svg, RIGHT)
+        self.play(Write(company_label))
 
-        # Wait 5 seconds before finishing
+        self.play(Create(get_svg_object(f"{REGULAR_FONT_AWESOME}/bell.svg")))
+
         self.wait(1)
